99853e8a8515953da898d16b189d7a913deec53a
Gabriel Meza Prada <standbyg2@gmail.com>
2023-11-03 18:16:56 -0500

    Fixing the error, Maybe

--- frontendSV64/frontendSV64/src/app/models/university.ts
+++
@@ -1,7 +0,0 @@
-export class University{
-    idUniversity:number=0
-    nameUniversity:string=""
-    adressUniversity:string=""
-    creationDateUniversity:Date=new Date(Date.now())
-    typeUniversity:string=""
-}
